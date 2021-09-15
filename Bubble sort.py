4d67f2a9c739a3bb49d08a6e69b52fe4656d6e26
Dillon-Dawson <87071332+Dillon-Dawson@users.noreply.github.com>
2021-09-15 13:05:48 +0100

    Add files via upload

--- Bubble sort.py
+++ Bubble sort.py
@@ -6,9 +6,13 @@ def bubble_sort(items): #This defines the section of the code that does the
     while swapped:
         swapped = False
         passes += 1
+        if Sort == True:
+            print(items)
         for j in range(1, last):
             if items[j - 1] > items[j]:
                 items[j], items[j - 1] = items[j - 1], items[j]  # Swap
+                if Sort == False:
+                    print(items)
                 changes += 1
                 swapped = True
                 last = j
@@ -20,9 +24,14 @@ def bubble_sort(items): #This defines the section of the code that does the
 
 
 print("Stretch_Beatz Bubble Sort Algorithm ")
-
+"""
 while True:
     print("Enter as many numbers as you want.\n You can choose between 0 and 9.\nLeave a space between each one")
     numbers = input()
     items = [int(num) for num in numbers.split() if num.isdigit()]
     if items: bubble_sort(items)
+"""
+Sort = True
+print (bubble_sort([50,25,5,20,10]))
+Sort = False
+print (bubble_sort([50,25,5,20,10]))
