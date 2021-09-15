190ddf85f2a3684929216606f0de8426ba3c76b2
Dillon-Dawson <87071332+Dillon-Dawson@users.noreply.github.com>
2021-09-15 14:26:14 +0100

    Add files via upload

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,37 +1,36 @@
-def bubble_sort(items): #This defines the section of the code that does the 
+
+def bubble_sort(items, swaps=True): #This defines the section of the code that does the 
     changes = passes = 0 # This sets the changes and passes to 0 every time this section is ran.
     last = len(items) #lens is a method that returns the length of a list.
     swapped = True
+    output = []
+    output += items
 
     while swapped:
         swapped = False
         passes += 1
-        if Sort == True:
-            print(items)
+        if swaps == True:
+            #print(items)
+            output += items
         for j in range(1, last):
             if items[j - 1] > items[j]:
                 items[j], items[j - 1] = items[j - 1], items[j]  # Swap
-                if Sort == False:
-                    print(items)
+                if swaps == False:
+                    #print(items)
+                    output += items
                 changes += 1
                 swapped = True
                 last = j
+    return output
+    if changes == 0:
+        print("data presorted")
 
+def main():
+    print("Stretch_Beatz Bubble Sort Algorithm")
+    print ("Test 1",  bubble_sort([50,25,5,20,10]))
+    print ("Test 2", bubble_sort([50,25,5,20,10], swaps=False))
+    print ("Test 3", bubble_sort([5,5,5,5,5], swaps=False))
 
-    print(items)
-    print("Number of passes =",passes)
-    print("Number of swaps =",changes)
+if __name__ == "__main__":
+    main()
 
-
-print("Stretch_Beatz Bubble Sort Algorithm ")
-"""
-while True:
-    print("Enter as many numbers as you want.\n You can choose between 0 and 9.\nLeave a space between each one")
-    numbers = input()
-    items = [int(num) for num in numbers.split() if num.isdigit()]
-    if items: bubble_sort(items)
-"""
-Sort = True
-print (bubble_sort([50,25,5,20,10]))
-Sort = False
-print (bubble_sort([50,25,5,20,10]))
